aacc7c895a7cb33f2156df6d22c8b744502b7ce9
rokoasusmous <135190801+rokoasusmous@users.noreply.github.com>
2023-05-31 22:35:24 -0500

    Add files via upload

---
+++ HttpListenerConsole/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Threading;
+using System.Linq.Expressions;
+using System.Net.Sockets;
+
+namespace HttpListenerConsole
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            HttpListener listener = null;
+            try
+            {
+                listener = new HttpListener();
+                listener.Prefixes.Add("http://localhost:8080/");
+                listener.Start();
+                while (true)
+                {
+                    Console.WriteLine("waiting...");
+                    HttpListenerContext context = listener.GetContext();
+                    string msg = "Hello!";
+                    context.Response.ContentLength64 = Encoding.UTF8.GetByteCount(msg);
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+                    using (Stream stream = context.Response.OutputStream)
+                    {
+                        using (StreamWriter writer = new StreamWriter(stream))
+                        {
+                            writer.Write(msg);
+                        }
+                    }
+                    Console.WriteLine("msg sent...");
+                }
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine(e.Status);
+            }
+        }
+    }
+}
